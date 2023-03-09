b1f974a8118d80741fa90471780d29ac0ac421d8
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-03-09 04:21:41 -0300

    Estilos

--- src/components/SearchBar/styledComponents.js
+++ src/components/SearchBar/styledComponents.js
@@ -3,23 +3,26 @@ import styled from "styled-components";
 export const Bar = styled.div`
    width: 20em;
    display:flex;
-   justify-content: space-between;
+   justify-content: end;
 `;
 
 export const Input = styled.div`
    border:0px;
-   border-radius: 15px 0px 0px 15px;
+   border-radius: 5px 0px 0px 5px;
    background-color: white;
-   width: 11.8em;   
+   width: 10em;
+   height: 2.34em;
+   box-shadow: 0px 4px 12px 5px rgb(31,217,242);
 `;
 
 export const Boton = styled.div`
-   border:0px;
-   border-radius: 0px 15px 15px 0px;
+   border-left: 1px solid black;
+   border-radius: 0px 5px 5px 0px;
    background-color: white;
+   padding-top: 0.3em;
    width: 6em;
    height: 2em;
-   padding-right: 2em;
-   display:flex;
-   justify-content: center;
+   display: flex;
+   justify-content: center;   
+   box-shadow: 0px 4px 12px 5px rgb(31,217,242);
 `;
